17f25f2a6d5bdf0e2a2c431febd5f81449a3ac29
oriel blackmark <kiplangatoriel@gmail.com>
2024-08-16 02:14:02 +0300

    Modified: animation effect for about section

--- my-project/src/components/Hero.jsx
+++ my-project/src/components/Hero.jsx
@@ -44,7 +44,13 @@ const Hero = () => {
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <img src={profilepic} alt="Oriel Kiplangat" />
+            <motion.img
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.2 }}
+              src={profilepic}
+              alt="Oriel Kiplangat"
+            />
           </div>
         </div>
       </div>

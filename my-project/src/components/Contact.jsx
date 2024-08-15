637044e7f42e37315e7a3c1e1af179eda3d45af7
oriel blackmark <kiplangatoriel@gmail.com>
2024-08-16 01:04:28 +0300

    Added: contact.jsx

---
+++ my-project/src/components/Contact.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-20">
+      <h2 className="my-10 text-center text-4xl"> Contact</h2>
+    </div>
+  ); 
+};
+
+export default Contact;

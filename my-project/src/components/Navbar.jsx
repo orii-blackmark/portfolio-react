37c9a73db6a3efe8165c6c54aebaf5414a74fc6c
oriel blackmark <kiplangatoriel@gmail.com>
2024-08-15 22:10:25 +0300

    Added the hero component

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import { FaLinkedin, FaGithub, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav className=" bg-red-900 mb-20 flex items-center justify-between py-4">
+    <nav className=" mb-20 flex items-center justify-between py-4">
       <div className="flex flex-shrink-0 items-center">
-        <img src={logo} alt="logo" />
+        <img className="mx-2 w-10 " src={logo} alt="logo" />
       </div>
-      <div className="m-4 flex items-center justify-center gap-6 text-2xl">
+      <div className="m-4 flex items-center justify-center gap-6 text-1xl">
         <FaLinkedin />
         <FaGithub />
         <FaTwitter />

bbe1d78926025d5bef38f88a9b5189f597ab377d
oriel blackmark <kiplangatoriel@gmail.com>
2024-08-17 20:09:56 +0300

    Fixed the padding issue

--- my-project/src/components/Hero.jsx
+++ my-project/src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
               variants={container(0)}
               initial="hidden"
               animate="visible"
-              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
+              className="pb-10 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
             >
               Oriel Kiplangat
             </motion.h1>
